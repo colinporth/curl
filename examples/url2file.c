a3b591ec78db7bff1944aa8034b6194efe221765
colinporth <colinwrey@hotmail.com>
2019-12-31 17:49:54 +0000

    fiddle

--- examples/url2file.c
+++ examples/url2file.c
@@ -31,7 +31,7 @@
 
 static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
 {
-  size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
+  size_t written = fwrite (ptr, size, nmemb, (FILE *)stream);
   return written;
 }
 
@@ -46,39 +46,39 @@ int main(int argc, char *argv[])
     return 1;
   }
 
-  curl_global_init(CURL_GLOBAL_ALL);
+  curl_global_init (CURL_GLOBAL_ALL);
 
   /* init the curl session */
   curl_handle = curl_easy_init();
 
   /* set URL to get here */
-  curl_easy_setopt(curl_handle, CURLOPT_URL, argv[1]);
+  curl_easy_setopt (curl_handle, CURLOPT_URL, argv[1]);
 
   /* Switch on full protocol/debug output while testing */
-  curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
+  curl_easy_setopt (curl_handle, CURLOPT_VERBOSE, 1L);
 
   /* disable progress meter, set to 0L to enable and disable debug output */
-  curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
+  curl_easy_setopt (curl_handle, CURLOPT_NOPROGRESS, 1L);
 
   /* send all data to this function  */
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
+  curl_easy_setopt (curl_handle, CURLOPT_WRITEFUNCTION, write_data);
 
   /* open the file */
-  pagefile = fopen(pagefilename, "wb");
-  if(pagefile) {
+  pagefile = fopen (pagefilename, "wb");
+  if (pagefile) {
 
     /* write the page body to this file handle */
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, pagefile);
+    curl_easy_setopt (curl_handle, CURLOPT_WRITEDATA, pagefile);
 
     /* get it! */
-    curl_easy_perform(curl_handle);
+    curl_easy_perform (curl_handle);
 
     /* close the header file */
     fclose(pagefile);
   }
 
   /* cleanup curl stuff */
-  curl_easy_cleanup(curl_handle);
+  curl_easy_cleanup (curl_handle);
 
   curl_global_cleanup();
 
